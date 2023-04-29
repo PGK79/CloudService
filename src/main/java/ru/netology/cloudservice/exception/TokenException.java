0a141831aedc84b2b87f44265c25e61998a500a7
PGK79 <pgk_nev@mail.ru>
2023-04-29 20:39:58 +0300

    Создан класс исключения при неверном токене

---
+++ src/main/java/ru/netology/cloudservice/exception/TokenException.java
@@ -0,0 +1,11 @@
+package ru.netology.cloudservice.exception;
+
+import lombok.Data;
+
+@Data
+public class TokenException extends RuntimeException{
+    public TokenException(String msg) {
+        super(msg);
+    }
+}
+
