d917f3a954e80e3812a51c0934f26ca50b495b71
PGK79 <pgk_nev@mail.ru>
2023-04-29 20:49:01 +0300

    Создан класс Контроллера с первым методом -POST /login

---
+++ src/main/java/ru/netology/cloudservice/controller/CloudController.java
@@ -0,0 +1,29 @@
+package ru.netology.cloudservice.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.cloudservice.model.AuthorizeData;
+import ru.netology.cloudservice.model.Login;
+import ru.netology.cloudservice.service.FileService;
+import ru.netology.cloudservice.service.UserService;
+
+@RestController
+public class CloudController {
+    private final UserService userService;
+    private final FileService fileService;
+
+    public CloudController(UserService userService, FileService fileService) {
+        this.userService = userService;
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/login")
+    public Login login(@RequestBody AuthorizeData authorizeData) {
+        return userService.login(authorizeData.getLogin(), authorizeData.getPassword());
+    }
+}
+
+
+
+
