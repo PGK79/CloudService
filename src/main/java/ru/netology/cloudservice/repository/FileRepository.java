6c455f2839d1bfad4fa580e02b581b54f3d1b808
PGK79 <pgk_nev@mail.ru>
2023-04-29 20:25:29 +0300

    Создан интерфейс FileRepository

---
+++ src/main/java/ru/netology/cloudservice/repository/FileRepository.java
@@ -0,0 +1,20 @@
+package ru.netology.cloudservice.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.netology.cloudservice.entity.File;
+import ru.netology.cloudservice.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findFileByNameAndUser(String filename, User user);
+
+    List<File> findAllFilesByUser(User user, Pageable pageable);
+
+
+}
