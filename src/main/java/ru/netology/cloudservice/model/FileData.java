a36604cddf828dee4dbe497c4c23cef5b8cb010a
PGK79 <pgk_nev@mail.ru>
2023-04-30 00:10:28 +0300

    Добавлен конструктор без аргументов

--- src/main/java/ru/netology/cloudservice/model/FileData.java
+++ src/main/java/ru/netology/cloudservice/model/FileData.java
@@ -2,10 +2,13 @@ package ru.netology.cloudservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FileData{
     private String filename;
     private long size;
+
 }
