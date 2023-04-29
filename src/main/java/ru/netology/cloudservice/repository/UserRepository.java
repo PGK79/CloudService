3d52885772904b44d85c146d48b826f849efc04a
PGK79 <pgk_nev@mail.ru>
2023-04-29 20:27:22 +0300

    Убран запасной метод из UserRepository

--- src/main/java/ru/netology/cloudservice/repository/UserRepository.java
+++ src/main/java/ru/netology/cloudservice/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package ru.netology.cloudservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.cloudservice.entity.User;
 
@@ -13,9 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginAndPassword(String login, String password);
 
     Optional<User> findUserByAuthToken(String authToken);
-
-    @Query("select u from User u where u.authToken = :token")
-    Optional<User> findUser(@Param("token") String name);
 
 
 
