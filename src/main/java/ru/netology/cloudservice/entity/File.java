65846030f7bd62bbea1db4318e45e42027cf3863
PGK79 <pgk_nev@mail.ru>
2023-04-29 20:36:52 +0300

    Создан entity File

---
+++ src/main/java/ru/netology/cloudservice/entity/File.java
@@ -0,0 +1,66 @@
+package ru.netology.cloudservice.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "content")
+    private byte[] content;
+
+    @Column(name = "size")
+    private long size;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public File(byte[] content, long size, String name, User user) {
+        this.content = content;
+        this.size = size;
+        this.name = name;
+        this.user = user;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
