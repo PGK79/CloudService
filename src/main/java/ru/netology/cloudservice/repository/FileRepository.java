44024195471c9d9642a249f6bf85dcab23cf7865
PGK79 <pgk_nev@mail.ru>
2023-04-30 16:43:56 +0300

    Изменено имя entity

--- src/main/java/ru/netology/cloudservice/repository/FileRepository.java
+++ src/main/java/ru/netology/cloudservice/repository/FileRepository.java
@@ -3,17 +3,17 @@ package ru.netology.cloudservice.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.netology.cloudservice.entity.File;
+import ru.netology.cloudservice.entity.FileEntity;
 import ru.netology.cloudservice.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FileRepository extends JpaRepository<File, Long> {
-    Optional<File> findFileByNameAndUser(String filename, User user);
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
+    Optional<FileEntity> findFileByNameAndUser(String filename, User user);
 
-    List<File> findAllFilesByUser(User user, Pageable pageable);
+    List<FileEntity> findAllFilesByUser(User user, Pageable pageable);
 
 
 }
