bb61176b6c61033ede5b98a3c700c38082c279c9
PGK79 <pgk_nev@mail.ru>
2023-04-30 16:38:33 +0300

    Изменено имя entity файла для удобства верификации model и entity

--- src/main/java/ru/netology/cloudservice/entity/FileEntity.java
+++ src/main/java/ru/netology/cloudservice/entity/FileEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @Table(name = "files")
-public class File {
+public class FileEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -25,7 +25,7 @@ public class File {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    public File(byte[] content, long size, String name, User user) {
+    public FileEntity(byte[] content, long size, String name, User user) {
         this.content = content;
         this.size = size;
         this.name = name;
