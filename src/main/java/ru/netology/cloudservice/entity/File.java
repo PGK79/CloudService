ddd76525e7e4e24e2f6ed28395216098465b5ab9
PGK79 <pgk_nev@mail.ru>
2023-04-30 21:46:55 +0300

    Переименование FileEntity в File

--- src/main/java/ru/netology/cloudservice/entity/File.java
+++ src/main/java/ru/netology/cloudservice/entity/File.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Data
 @Table(name = "files")
-public class FileEntity {
+public class File {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -27,7 +27,7 @@ public class FileEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    public FileEntity(byte[] content, long size, String name, User user) {
+    public File(byte[] content, long size, String name, User user) {
         this.content = content;
         this.size = size;
         this.name = name;
