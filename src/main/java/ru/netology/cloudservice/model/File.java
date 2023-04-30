55f40fe0939eb3a2a15d5f0dbca9bba9be309cec
PGK79 <pgk_nev@mail.ru>
2023-04-30 19:50:29 +0300

    Поле типа MultipartFile замено на байтовый массив

--- src/main/java/ru/netology/cloudservice/model/File.java
+++ src/main/java/ru/netology/cloudservice/model/File.java
@@ -3,13 +3,10 @@ package ru.netology.cloudservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 public final class File{
     private String hash;
-    private MultipartFile file;
+    private byte[] file;
 }
