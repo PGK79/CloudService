c6edc2c21380c140f3f50b56abc6b2e55bc81f24
PGK79 <pgk_nev@mail.ru>
2023-05-01 12:27:16 +0300

    reformat code and import

--- src/main/java/ru/netology/cloudservice/config/MultipartConfig.java
+++ src/main/java/ru/netology/cloudservice/config/MultipartConfig.java
@@ -1,35 +1,11 @@
 package ru.netology.cloudservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
-//@EnableWebMvc
 public class MultipartConfig {
-//public class MultipartConfig implements WebMvcConfigurer {
-
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//
-//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-//        converter.setObjectMapper(new ObjectMapper());
-//        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON,
-//                MediaType.MULTIPART_FORM_DATA));
-//        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON));
-//
-//        converters.add(converter);
-//    }
-
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
