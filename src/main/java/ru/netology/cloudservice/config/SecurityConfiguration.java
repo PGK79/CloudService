e4e8f5e782b5a7273bcbf02521aecf1f7289dedf
PGK79 <pgk_nev@mail.ru>
2023-05-01 12:26:34 +0300

    Добавлен конфигурационный класс безопасности

---
+++ src/main/java/ru/netology/cloudservice/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package ru.netology.cloudservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
+
