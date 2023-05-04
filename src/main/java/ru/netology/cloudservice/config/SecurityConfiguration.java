e1d081c542bc7fc7f22c77f683c4ef7503ec85ff
PGK79 <pgk_nev@mail.ru>
2023-05-04 18:27:24 +0300

    Настройка аутентификации через БД

--- src/main/java/ru/netology/cloudservice/config/SecurityConfiguration.java
+++ src/main/java/ru/netology/cloudservice/config/SecurityConfiguration.java
@@ -1,16 +1,24 @@
 package ru.netology.cloudservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
 
+    public SecurityConfiguration(@Autowired DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -20,6 +28,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
