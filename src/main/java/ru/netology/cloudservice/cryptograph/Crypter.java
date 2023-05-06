ddb637330f7e260c7fd028feb083998ebbe742b7
PGK79 <pgk_nev@mail.ru>
2023-05-06 19:51:17 +0300

    Смена имени класса, удаление метода шифрования

---
+++ src/main/java/ru/netology/cloudservice/cryptograph/Crypter.java
@@ -0,0 +1,27 @@
+package ru.netology.cloudservice.cryptograph;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Crypter {
+    private static final String key = "aesEncryptionKey";
+    private static final String initVector = "encryptionIntVec";
+    public static String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
