93d540c5ae43e2bb5172a6ccb41b38c665a262f2
PGK79 <pgk_nev@mail.ru>
2023-05-10 18:40:52 +0300

    Добавлены конструкторы

--- src/main/java/ru/netology/cloudservice/model/AuthorizeData.java
+++ src/main/java/ru/netology/cloudservice/model/AuthorizeData.java
@@ -1,8 +1,12 @@
 package ru.netology.cloudservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorizeData {
     private String login;
     private String password;
