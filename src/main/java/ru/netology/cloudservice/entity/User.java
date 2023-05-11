b6bb67a364c120288ac90f47da40738292a56a60
PGK79 <pgk_nev@mail.ru>
2023-05-11 23:57:29 +0300

    Удалены лишние поля

--- src/main/java/ru/netology/cloudservice/entity/User.java
+++ src/main/java/ru/netology/cloudservice/entity/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -24,13 +23,9 @@ public class User {
     @Column(name = "auth_token")
     private String authToken;
 
-    @OneToMany(mappedBy = "user")
-    private List<File> fileEntities;
-
-    public User(String login, String password, String authToken, List<File> fileEntities) {
+    public User(String login, String password, String authToken) {
         this.login = login;
         this.password = password;
         this.authToken = authToken;
-        this.fileEntities = fileEntities;
     }
 }
