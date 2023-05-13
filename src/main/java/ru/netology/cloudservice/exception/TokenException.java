3f0a9eb5f7b736e37b4541fc84b92f2bc2b2f009
PGK79 <pgk_nev@mail.ru>
2023-05-13 21:37:33 +0300

    reformat code

--- src/main/java/ru/netology/cloudservice/exception/TokenException.java
+++ src/main/java/ru/netology/cloudservice/exception/TokenException.java
@@ -3,9 +3,8 @@ package ru.netology.cloudservice.exception;
 import lombok.Data;
 
 @Data
-public class TokenException extends RuntimeException{
+public class TokenException extends RuntimeException {
     public TokenException(String msg) {
         super(msg);
     }
 }
-
