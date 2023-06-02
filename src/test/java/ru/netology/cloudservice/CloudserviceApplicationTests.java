5d09cc3306dca31426cc4f4f0f8c01502946fcc5
PGK79 <pgk_nev@mail.ru>
2023-06-02 21:02:11 +0300

    Добавлены тестовые методы

--- src/test/java/ru/netology/cloudservice/CloudserviceApplicationTests.java
+++ src/test/java/ru/netology/cloudservice/CloudserviceApplicationTests.java
@@ -7,11 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
 import ru.netology.cloudservice.cryptograph.Crypter;
 import ru.netology.cloudservice.entity.File;
 import ru.netology.cloudservice.entity.User;
@@ -22,6 +26,7 @@ import ru.netology.cloudservice.repository.FileRepository;
 import ru.netology.cloudservice.repository.UserRepository;
 import ru.netology.cloudservice.service.UserService;
 
+import javax.sql.DataSource;
 import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Map;
@@ -29,14 +34,14 @@ import java.util.Map;
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudserviceApplicationTests {
-    private static final Network network = Network.newNetwork();
+    private final String filename = "filename";
     private String login = "ivan@mail.com";
     private final String password = "ivan";
     private final String token = "token";
-    private final String filename = "filename";
-    private final User user = new User(login, password, token);
-    private final File fileOne = new File("file content".getBytes(), 12L, filename, user);
-    private final File fileTwo = new File("file content two".getBytes(), 16L, "new" + filename, user);
+    private final User user = new User(1L,login, password, token);
+
+    private final File fileOne = new File(1,"file content".getBytes(), 12L, filename, user);
+    private final File fileTwo = new File(2,"file content two".getBytes(), 16L, "new" + filename, user);
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -50,47 +55,40 @@ class CloudserviceApplicationTests {
     @Autowired
     UserService userService;
 
-
-    @BeforeEach
-    void setUp() {
-        userRepository.deleteAll();
-        fileRepository.deleteAll();
-    }
-
     @Container
-    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql")
-            .withNetwork(network)
-            .withExposedPorts(3306)
+    private static MySQLContainer<?> database = new MySQLContainer<>("mysql")
             .withDatabaseName("cloud_database")
             .withUsername("test")
-            .withPassword("test");
+            .withPassword("test")
+            .withInitScript("db.sql");
 
-    @Container
-    private static final GenericContainer<?> backendApp =
-            new GenericContainer<>("cloudbackend:1.0")
-                    .withExposedPorts(8081)
-                    .dependsOn(mysql)
-                    .withEnv(Map.of("SPRING_DATASOURCE_URL", "jdbc:mysql://localhost:3306/cloud_database"))
-                    .withNetwork(network);
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+    }
+
+
+
+//    @BeforeEach
+//    void setUp() {
+//        userRepository.deleteAll();
+//        fileRepository.deleteAll();
+//    }
 
     @Test
     void contextDatabase() {
-        Assertions.assertTrue(mysql.isRunning());
-    }
-
-    @Test
-    void contextServer() {
-        Assertions.assertFalse(backendApp.isRunning());
+        Assertions.assertTrue(database.isRunning());
     }
 
     @Test
     void testGetUserByLoginAndPassword() {
         //given
-        userRepository.save(user);
         User expected = user;
 
         // when:
-        User actual = userRepository.findByLoginAndPassword("test@mail.com", "test").get();
+        User actual = userRepository.findByLoginAndPassword("ivan@mail.com", "ivan").get();
 
         // then:
         Assertions.assertEquals(expected, actual);
@@ -99,7 +97,7 @@ class CloudserviceApplicationTests {
     @Test
     void testGetUserByAuthToken() {
         //given
-        userRepository.save(user);
+
         User expected = user;
 
         // when:
@@ -112,8 +110,7 @@ class CloudserviceApplicationTests {
     @Test
     void testGetFileByNameAndUser() {
         //given
-        userRepository.save(user);
-        fileRepository.save(fileOne);
+        //fileRepository.save(fileOne);
         File expected = fileOne;
 
         // when:
@@ -126,9 +123,7 @@ class CloudserviceApplicationTests {
     @Test
     void testGetFile() {
         //given
-        userRepository.save(user);
-        fileRepository.save(fileOne);
-        fileRepository.save(fileTwo);
+        //fileRepository.save(fileTwo);
         List<File> expected = List.of(fileOne, fileTwo);
 
         // when:
@@ -161,7 +156,6 @@ class CloudserviceApplicationTests {
         User userWithoutToken = new User(login, Crypter.encrypt(password));
         login = "petr@mail.com";
         userRepository.save(userWithoutToken);
-        Login expected = new Login(token);
 
         // then:
         Assertions.assertThrows(AuthorizeException.class, () -> userService.login(new AuthorizeData(login, password)));
